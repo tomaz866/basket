bbff631c5836ab882d950de3974aa18a5c8cfd72
“tomaz866 <twlodarski86@gmail.com>
2018-02-15 12:50:07 +0100

    1.4

---
+++ src/main/java/com/example/koszyk/domain/Storage.java
@@ -0,0 +1,34 @@
+package com.example.koszyk.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "STORAG")
+@Getter
+public class Storage {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "ITEM")
+    private String name;
+
+    @Column(name = "PRICE")
+    private double price;
+
+    @Column(name = "UNIT")
+    private int unit;
+
+    @Column(name = "SPECIAL_PRICE")
+    private double specialPrice;
+}
---
+++ src/main/java/com/example/koszyk/repository/StorageRepository.java
@@ -0,0 +1,7 @@
+package com.example.koszyk.repository;
+
+import com.example.koszyk.domain.Storage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StorageRepository extends CrudRepository<Storage,Long> {
+}
