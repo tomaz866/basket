8302a833e6e0e55696742a84c1843b01ef3d6735
tomaz866 <twlodarski86@gmail.com>
2018-03-07 11:09:01 +0100

    Rename

--- src/main/java/com/example/koszyk/BasketApplication.java
+++ src/main/java/com/example/koszyk/BasketApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KoszykApplication {
+public class BasketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KoszykApplication.class, args);
+		SpringApplication.run(BasketApplication.class, args);
 	}
 }
